2eb80b182860ac49422a1fbc09aa8ec832555d69
s1f102202280 <s1f102202280@iniad.org>
2026-01-09 18:32:38 +0900

    メインページの作成

--- src/main/java/com/example/stock_trading_management/MenuController.java
+++ src/main/java/com/example/stock_trading_management/MenuController.java
@@ -8,16 +8,45 @@ public class MenuController {
 
     @GetMapping("/")
     public String index() {
-        // ブラウザに表示される内容です
         return """
             <h1>株式取引管理システム</h1>
             <p>操作するメニューを選んでください。</p>
             <ul>
-                <li>A: 株式マスタ一覧表示</li>
-                <li>B: 銘柄マスタ新規登録</li>
-                <li>C: 取引入力</li>
-                <li>Q: アプリケーションを終了する</li>
+                <li><a href="/printStocks">A: 株式マスタ一覧表示</a></li>
+                <li><a href="/registrationNewStock">B: 銘柄マスタ新規登録</a></li>
+                <li><a href="/registrationTraded">C: 取引入力</a></li>
+                <li>Q: アプリケーションを終了する (ブラウザを閉じればOK)</li>
             </ul>
+            """;
+    }
+
+    @GetMapping("/printStocks")
+    public String printStocks() {
+        return """
+            <h2>株式銘柄一覧</h2>
+            <p>株式銘柄一覧が選択されました。</p>
+            <br>
+            <a href="/">メニューに戻る</a>
+            """;
+    }
+
+    @GetMapping("/registrationTraded")
+    public String registrationTraded() {
+        return """
+            <h2>「取引入力」が選択されました</h2>
+            <p>取引情報を入力します</p>
+            <br>
+            <a href="/">メニューに戻る</a>
+            """;
+    }
+
+    @GetMapping("/registrationNewStock")
+    public String registrationNewStock() {
+        return """
+            <h2>「銘柄マスタ新規登録」が選択されました</h2>
+            <p>新規株式銘柄マスタを登録します</p>
+            <br>
+            <a href="/">メニューに戻る</a>
             """;
     }
 }
